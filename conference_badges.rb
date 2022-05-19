db64505b7369867ea4580a75b17a62e2afeb362e
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-19 16:04:39 -0700

    commit tree e9f4eea3f958d7bcbfd1180b01ff5d0d244a589a

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,17 +5,26 @@ def badge_maker(name)
     return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(arr)
-    new_arr = []
-    arr.map {|name| new_arr << "Hello, my name is #{name}."}
-    return new_arr
+
+##WITH EACH
+# def batch_badge_creator(names)
+#     new_badges = []
+#     names.each {|name| new_badges << "Hello, my name is #{name}."}
+#     return new_badges
+# end
+
+def batch_badge_creator(names)
+    names.map {|name| "Hello, my name is #{name}."}
 end
 
-def assign_rooms(speakers)
-    speakers.map.with_index(1) {|speaker, room| "Hello, #{speaker}! You'll be assigned to room #{room}!"}
+def assign_rooms(names)
+    assigned_rooms = names.map.with_index(1) {|name, room| "Hello, #{name}! You'll be assigned to room #{room}!"}
+    assigned_rooms
 end
 
-def printer(speakers)
-    batch_badge_creator(speakers).each {|badge| puts badge}
-    assign_rooms(speakers).each {|room| puts room}
+
+
+def printer(names)
+    batch_badge_creator(names).each {|name| puts name}
+    assign_rooms(names).each {|room| puts room}
 end
